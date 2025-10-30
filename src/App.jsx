692c365761fc27875e756b7ef72bac62198245bc
Omar Akhter <umar.farooq@fabtechsol.com>
2025-10-30 16:05:09 +0500

    Add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import HeroSection from './components/HeroSection';
+import PortfolioSection from './components/PortfolioSection';
+import './App.css';
+
+function App() {
+ return (
+ <div className="App">
+ <HeroSection />
+ <PortfolioSection />
+ </div>
+ );
+}
+
+export default App;
